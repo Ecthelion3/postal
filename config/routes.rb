3d9766df4b6ed613462c705ba0c8256806d252f7
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-11 12:03:02 +0200

    User's posts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
+  resources :posts
+
   root 'posts#index'
 
   get 'posts/index'
-
-  resources :posts
+  get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
